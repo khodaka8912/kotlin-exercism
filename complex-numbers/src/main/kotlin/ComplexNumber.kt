a6f57042083a711597d2859df283d21af170ecdb
hwatanabe <hodaka.hw.watanabe@nts.ricoh.co.jp>
2019-08-27 18:54:30 +0900

    Done list-ops, complex-numbers

---
+++ complex-numbers/src/main/kotlin/ComplexNumber.kt
@@ -0,0 +1,26 @@
+import kotlin.math.*
+
+data class ComplexNumber(val real: Double = 0.0, val imag: Double = 0.0) {
+
+    operator fun plus(other: ComplexNumber) = ComplexNumber(real + other.real, imag + other.imag)
+
+    operator fun minus(other: ComplexNumber) = ComplexNumber(real - other.real, imag - other.imag)
+
+    operator fun times(other: ComplexNumber) =
+            ComplexNumber(real * other.real - imag * other.imag, real * other.imag + imag * other.real)
+
+
+    operator fun div(other: ComplexNumber) =
+            ComplexNumber((real * other.real + imag * other.imag) / other.square(),
+                    (imag * other.real - real * other.imag) / other.square())
+
+    val abs: Double
+        get() = sqrt(square())
+
+    fun conjugate() = ComplexNumber(real, -imag)
+
+
+    private fun square() = real * real + imag * imag
+}
+
+fun exponential(num: ComplexNumber) = ComplexNumber(E.pow(num.real) * cos(num.imag), E.pow(num.real) * sin(num.imag))
---
+++ list-ops/src/main/kotlin/ListExtentions.kt
@@ -0,0 +1,62 @@
+fun <E> List<E>.customAppend(list: List<E>): List<E> {
+    val appended = mutableListOf<E>()
+    appended.addAll(this)
+    appended.addAll(list)
+    return appended
+}
+
+fun <E> List<List<E>>.customConcat(): List<E> {
+    val concatenated = mutableListOf<E>()
+    for (list in this) {
+        concatenated.addAll(list)
+    }
+    return concatenated
+}
+
+fun <E> List<E>.customFilter(predicate: (E) -> Boolean): List<E> {
+    val filtered = mutableListOf<E>()
+    forEach {
+        if (predicate(it)) {
+            filtered.add(it)
+        }
+    }
+    return filtered
+}
+
+
+val <E> List<E>.customSize: Int
+    get() {
+        var count = 0
+        forEach { _ -> count++ }
+        return count
+    }
+
+fun <T, R> List<T>.customMap(function: (T) -> R): List<R> {
+    val mapped = mutableListOf<R>()
+    forEach { mapped.add(function(it)) }
+    return mapped
+}
+
+fun <T, E> List<E>.customFoldLeft(initial: T, function: (T, E) -> T): T {
+    var result = initial
+    forEach { result = function(result, it) }
+    return result
+}
+
+fun <T, E> List<E>.customFoldRight(initial: T, function: (E, T) -> T): T {
+    var result = initial
+    val ite = listIterator(customSize)
+    while (ite.hasPrevious()) {
+        result = function(ite.previous(), result)
+    }
+    return result
+}
+
+fun <E> List<E>.customReverse(): List<E> {
+    val reversed = mutableListOf<E>()
+    val ite = listIterator(customSize)
+    while (ite.hasPrevious()) {
+        reversed.add(ite.previous())
+    }
+    return reversed
+}
